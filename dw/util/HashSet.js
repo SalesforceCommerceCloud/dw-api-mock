fe1aa5a4b39f7f4ec219ca0570cbcb92578eb73b
ngeor <nikolaos.georgiou@bestseller.com>
2016-04-22 14:17:37 +0200

    Added HashSet.contains

--- dw/util/HashSet.js
+++ dw/util/HashSet.js
@@ -6,6 +6,10 @@ HashSet.prototype.add = function(key) {
     this.map[key] = key;
 };
 
+HashSet.prototype.contains = function(key) {
+    return this.map[key] === key;
+};
+
 HashSet.EMPTY_SET = new HashSet();
 
 module.exports = HashSet;
