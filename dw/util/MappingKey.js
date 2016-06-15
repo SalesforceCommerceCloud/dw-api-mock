4468f1a8f743d3ae7d39e15c7353c072bbd8912e
nkartelija <nkartelija@westum.com>
2016-06-15 12:51:41 +0200

    Added a Mapping API recently introduced by Demandware

---
+++ dw/util/MappingKey.js
@@ -0,0 +1,8 @@
+var MappingKey = function() {};
+
+MappingKey.prototype.getKeyComponents = function() {};
+
+MappingKey.prototype.getSingleComponentKey = function() {};
+
+MappingKey.prototype.keyComponents = null;
+MappingKey.prototype.singleComponentKey = null;
---
+++ dw/util/MappingMgr.js
@@ -0,0 +1,11 @@
+var MappingMgr = function() {};
+
+MappingMgr.get = function() {};
+
+MappingMgr.getFirst = function() {};
+
+MappingMgr.getMappingNames = function() {};
+
+MappingMgr.keyIterator = function() {};
+
+MappingMgr.mappingNames = null;
