d42ad5dfbf1eca00b83878bb4b0900b6b59bc57d
Holger Nestmann <hnestmann@demandware.com>
2016-08-08 10:36:57 +0200

    BING-1941: added csrf token stuff

---
+++ dw/web/CSRFProtection.js
@@ -0,0 +1,7 @@
+var CSRFProtection = function() {};
+
+CSRFProtection.generateToken = function() {};
+CSRFProtection.getTokenName = function() {};
+CSRFProtection.validateRequest = function() {};
+
+module.exports = CSRFProtection;
