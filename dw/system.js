f2d7089810377961e9fd783ae5460c96c8550b5f
Tobias Lohr <tlohr@demandware.com>
2016-11-18 14:21:18 +0100

    added Pipeline when requireing whole dw/system package

--- dw/system.js
+++ dw/system.js
@@ -1,6 +1,7 @@
 exports.AgentUserStatusCodes = require('./system/AgentUserStatusCodes.js');
 exports.JobProcessMonitor = require('./system/JobProcessMonitor.js');
 exports.OrganizationPreferences = require('./system/OrganizationPreferences.js');
+exports.Pipeline = require('./system/Pipeline.js');
 exports.Request = require('./system/Request.js');
 exports.Response = require('./system/Response.js');
 exports.Session = require('./system/Session.js');
