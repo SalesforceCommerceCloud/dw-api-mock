59f3057d44c6ccfb05a851adccb81dd99e2ee020
Danny Gehl <dgehl@salesforce.com>
2017-06-19 11:31:28 +0000

    ObjectAttributeDefinition.js edited online with Bitbucket

--- dw/object/ObjectAttributeDefinition.js
+++ dw/object/ObjectAttributeDefinition.js
@@ -22,4 +22,23 @@ ObjectAttributeDefinition.prototype.attributeGroups=null;
 ObjectAttributeDefinition.prototype.valueTypeCode=null;
 ObjectAttributeDefinition.prototype.values=null;
 
+ObjectAttributeDefinition.VALUE_TYPE_STRING = 0;
+ObjectAttributeDefinition.VALUE_TYPE_INT = 1;
+ObjectAttributeDefinition.VALUE_TYPE_NUMBER = 2;
+ObjectAttributeDefinition.VALUE_TYPE_TEXT = 3;
+ObjectAttributeDefinition.VALUE_TYPE_HTML = 4;
+ObjectAttributeDefinition.VALUE_TYPE_DATE = 5;
+ObjectAttributeDefinition.VALUE_TYPE_DATETIME = 6;
+ObjectAttributeDefinition.VALUE_TYPE_IMAGE = 7;
+ObjectAttributeDefinition.VALUE_TYPE_EMAIL = 8;
+ObjectAttributeDefinition.VALUE_TYPE_PASSWORD = 9;
+ObjectAttributeDefinition.VALUE_TYPE_BOOLEAN = 10;
+ObjectAttributeDefinition.VALUE_TYPE_MONEY = 11;
+ObjectAttributeDefinition.VALUE_TYPE_QUANTITY = 12;
+ObjectAttributeDefinition.VALUE_TYPE_SET_OF_STRING = 13;
+ObjectAttributeDefinition.VALUE_TYPE_SET_OF_INT = 14;
+ObjectAttributeDefinition.VALUE_TYPE_SET_OF_NUMBER = 15;
+ObjectAttributeDefinition.VALUE_TYPE_ENUM_OF_STRING = 16;
+ObjectAttributeDefinition.VALUE_TYPE_ENUM_OF_INT = 17;
+
 module.exports = ObjectAttributeDefinition;
