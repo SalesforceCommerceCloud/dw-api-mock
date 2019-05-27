1e47ff6c858c42d8cd10ac9128d466125d53faf6
kdomachowski <kdomachowski@salesforce.com>
2019-05-27 09:41:13 +0200

    Update BasketMgr.js
    fixing unresolved merge conflicts

--- dw/order/BasketMgr.js
+++ dw/order/BasketMgr.js
@@ -1,27 +1,16 @@
 var BasketMgr = function(){};
 
-<<<<<<< HEAD
-BasketMgr.prototype.createAgentBasket = function(){};
-BasketMgr.prototype.createBasketFromOrder = function(){};
-BasketMgr.prototype.deleteBasket = function(){};
-BasketMgr.prototype.getBasket = function(){};
-BasketMgr.prototype.getBaskets = function(){};
-BasketMgr.prototype.getCurrentBasket = function(){};
-BasketMgr.prototype.getCurrentOrNewBasket = function(){};
-BasketMgr.prototype.getStoredBasket = function(){};
-
-module.exports = new BasketMgr();
-=======
 BasketMgr.prototype.currentBasket = null;
 BasketMgr.prototype.currentOrNewBasket = null;
 BasketMgr.prototype.storedBasket = null;
 
+BasketMgr.createAgentBasket = function(){};
 BasketMgr.createBasketFromOrder = function(){};
+BasketMgr.deleteBasket = function(){};
 BasketMgr.getBasket = function(){};
+BasketMgr.getBaskets = function(){};
 BasketMgr.getCurrentBasket = function(){};
 BasketMgr.getCurrentOrNewBasket = function(){};
 BasketMgr.getStoredBasket = function(){};
 
-
 module.exports = BasketMgr;
->>>>>>> loreal/master
