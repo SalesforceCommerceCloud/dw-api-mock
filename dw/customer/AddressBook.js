4019f7188523765e2bac7eb0b451e773645a4a98
challmann <challmann@salesforce.com>
2019-10-15 08:47:30 +0200

    Update AddressBook mocks

--- dw/customer/AddressBook.js
+++ dw/customer/AddressBook.js
@@ -1,12 +1,18 @@
 var AddressBook = function(){};
 
 AddressBook.prototype.getAddress = function(){};
+AddressBook.prototype.getAddress1 = function(){};
+AddressBook.prototype.setAddress1 = function(){};
+AddressBook.prototype.getAddress2 = function(){};
+AddressBook.prototype.setAddress2 = function(){};
 AddressBook.prototype.createAddress = function(){};
 AddressBook.prototype.removeAddress = function(){};
 AddressBook.prototype.getPreferredAddress = function(){};
 AddressBook.prototype.getAddresses = function(){};
 AddressBook.prototype.setPreferredAddress = function(){};
 AddressBook.prototype.address=null;
+AddressBook.prototype.address1=null;
+AddressBook.prototype.address2=null;
 AddressBook.prototype.preferredAddress=null;
 AddressBook.prototype.addresses=null;
 
