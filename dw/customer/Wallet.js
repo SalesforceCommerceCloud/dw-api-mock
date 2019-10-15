11e42ed49114583a8ce5c5f4aee422282ca93527
challmann <challmann@salesforce.com>
2019-10-15 08:55:11 +0200

    Do not use constructor for wallet

--- dw/customer/Wallet.js
+++ dw/customer/Wallet.js
@@ -4,10 +4,10 @@ var Wallet = function(){};
 
 Wallet.prototype = new _super();
 
-Wallet.prototype.createPaymentInstrument = function(){};
-Wallet.prototype.getPaymentInstrument = function(){};
-Wallet.prototype.getPaymentInstruments = function(){};
-Wallet.prototype.removePaymentInstrument = function(){};
+Wallet.createPaymentInstrument = function(){};
+Wallet.getPaymentInstrument = function(){};
+Wallet.getPaymentInstruments = function(){};
+Wallet.removePaymentInstrument = function(){};
 
 
 Wallet.prototype.createPaymentInstrument=null;
